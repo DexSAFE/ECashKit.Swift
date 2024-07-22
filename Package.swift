679c53bd6e6e2d0963f5019e318568cc1bb693f4
DexSAFE <dexsafe.github@gmail.com>
2024-07-22 15:59:47 +0300

    init

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/horizontalsystems/BitcoinCashKit.Swift.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/DexSAFE/BitcoinCore.Swift.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/DexSAFE/BitcoinCashKit.Swift.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
